9a7b6b3f64fc31f43f17cc9c9b45d209e5fdc1c3
cactusaurelius <39528088+cactusaurelius@users.noreply.github.com>
2021-05-02 23:59:51 +0300

    session

---
+++ src/session.ts
@@ -0,0 +1,48 @@
+import { MyContext } from "./Telegram";
+
+interface SessionOptions {
+  property: string;
+  store?: Map<any, any> | any;
+  getSessionKey?: (ctx: MyContext) => string | boolean;
+  ttl?: number;
+}
+export default function TelegrafSession(opts: SessionOptions) {
+  const options = {
+    property: "session",
+    store: new Map(),
+    getSessionKey: (ctx: MyContext) =>
+      ctx.from && ctx.chat && `${ctx.from.id}:${ctx.chat.id}`,
+    ...opts,
+  };
+
+  const ttlMs = options.ttl && options.ttl * 1000;
+
+  return (ctx: MyContext, next: (A: any) => Promise<any>) => {
+    const key = options.getSessionKey(ctx);
+    if (!key) {
+      return next(ctx);
+    }
+    const now = new Date().getTime();
+    return Promise.resolve(options.store.get(key))
+      .then((state) => state || { session: {} })
+      .then(({ session, expires }) => {
+        if (expires && expires < now) {
+          session = {};
+        }
+        Object.defineProperty(ctx, options.property, {
+          get: function () {
+            return session;
+          },
+          set: function (newValue) {
+            session = { ...newValue };
+          },
+        });
+        return next(ctx).then(() =>
+          options.store.set(key, {
+            session,
+            expires: ttlMs ? now + ttlMs : null,
+          })
+        );
+      });
+  };
+}
